d8f5051ecb428453f10ab1e62fc4c4492ba1ce13
BbinMaharjan <bbinmaharjan@gmail.com>
2021-10-04 13:08:03 +0545

    COM# - initialize react strap

---
+++ src/pages/home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button } from "reactstrap";
+const Home = (props) => {
+  return (
+    <div>
+      <h1>Home</h1>
+      <Button color="primary">primary</Button>
+    </div>
+  );
+};
+
+export default Home;
