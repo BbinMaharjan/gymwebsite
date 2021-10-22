2a10d504c62d5b80be55df2bc43aa10f36227e38
BbinMaharjan <bbinmaharjan@gmail.com>
2021-10-22 20:12:59 +0545

    COM# -

--- src/pages/admin/gymOwners.js
+++ src/pages/admin/gymOwners.js
@@ -1,4 +1,5 @@
 import React from "react";
+import axios from "axios";
 
 import { Typography } from "@mui/material";
 
@@ -7,8 +8,23 @@ import Footer from "../../components/footer/footer";
 
 import Avatar from "react-avatar";
 import { Table } from "reactstrap";
+import { useEffect, useState } from "react";
+import Pagination from "@mui/material/Pagination";
+import Stack from "@mui/material/Stack";
+
+const BASE_URL = "https://reqres.in/api";
 
 const GymOwner = (props) => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  const getUsers = async () => {
+    const res = await axios.get(`${BASE_URL}/users?page=1`);
+    setUsers(res.data.data);
+  };
   return (
     <>
       <DrawerAdmin />
@@ -39,19 +55,26 @@ const GymOwner = (props) => {
               textAlign: "center",
             }}
           >
-            <tr>
-              <th scope="row">1</th>
-              <td>
-                {" "}
-                <Avatar name="Leon Hart" size="40" round={true} />
-              </td>
-              <td>Leon Hart</td>
-              <td>leon@gmail.com</td>
-              <td>kirtipur</td>
-              <td>9843221133</td>
-            </tr>
+            {users.map((users) => {
+              return (
+                <tr key={users.id}>
+                  <th scope="row">{users.id}</th>
+                  <td>
+                    {" "}
+                    <Avatar src={users.avatar} size="40" round={true} />
+                  </td>
+                  <td>{users.first_name}</td>
+                  <td>{users.email}</td>
+                  <td>{users.email}</td>
+                  <td>9843221133</td>
+                </tr>
+              );
+            })}
           </tbody>
         </Table>
+        <Stack spacing={2} alignItems="center">
+          <Pagination count={2} color="secondary" />
+        </Stack>
       </div>
       <Footer />
     </>
