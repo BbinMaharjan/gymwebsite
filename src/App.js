701daeec866f9dcab4866305fbb4ad02b09822a0
BbinMaharjan <bbinmaharjan@gmail.com>
2021-11-15 14:04:36 +0545

    COM# - private routes

--- src/App.js
+++ src/App.js
@@ -6,12 +6,11 @@ import Home from "./pages/home/home";
 import Login from "./pages/admin/login";
 import RegisterAdmin from "./pages/admin/register";
 
-import AdminRoutes from "./routes/adminroutes";
+import PrivateRoutes from "./routes/adminroutes";
 
 import GymOwnerLogin from "./pages/client/gymOwnerLogin";
 import RegisterGymOwner from "./pages/client/registerGymOwner";
 
-import GymOwnerRoute from "./routes/gymownerroutes";
 function App() {
   return (
     <BrowserRouter>
@@ -19,11 +18,13 @@ function App() {
         <Route exact path="/" component={Home} />
         <Route exact path="/admin/login" component={Login} />
         <Route exact path="/admin/register" component={RegisterAdmin} />
-        <Route exact path="/admin/dashboard" component={AdminRoutes} />
 
         <Route exact path="/gymowner/login" component={GymOwnerLogin} />
         <Route exact path="/gymowner/register" component={RegisterGymOwner} />
-        <Route exact path="/gymowner/dashboard" component={GymOwnerRoute} />
+
+        <Route>
+          <PrivateRoutes />
+        </Route>
       </Switch>
     </BrowserRouter>
   );
--- src/routes/adminroutes.js
+++ src/routes/adminroutes.js
@@ -6,20 +6,33 @@ import GymMembers from "../pages/admin/gymmembers";
 import GymOwner from "../pages/admin/gymOwners";
 import GymExercise from "../pages/admin/gymExercise";
 import Profile from "../pages/admin/profile";
+import DashboardGymOwner from "../pages/client/dashboardGymOwner";
 
 const AdminRoutes = (props) => {
-  if (!localStorage.getItem("token")) {
-    return (window.location.href = "/");
-  }
   return (
     <BrowserRouter>
-      <Switch>
-        <Route exact path="/admin/dashboard" component={Dashboard} />
-        <Route exact path="/admin/gymowners" component={GymOwner} />
-        <Route exact path="/admin/gymmembers" component={GymMembers} />
-        <Route exact path="/admin/gymexercises" component={GymExercise} />
-        <Route exact path="/admin/profile" component={Profile} />
-      </Switch>
+      {localStorage.getItem("token") ? (
+        <Switch>
+          <Route exact path="/admin/dashboard" component={Dashboard} />
+          <Route exact path="/admin/gymowners" component={GymOwner} />
+          <Route exact path="/admin/gymmembers" component={GymMembers} />
+          <Route exact path="/admin/gymexercises" component={GymExercise} />
+          <Route exact path="/admin/profile" component={Profile} />
+        </Switch>
+      ) : (
+        (window.location.href = "/")
+      )}
+      {sessionStorage.getItem("GymOwner") ? (
+        <Switch>
+          <Route
+            exact
+            path="/gymowner/dashboard"
+            component={DashboardGymOwner}
+          />
+        </Switch>
+      ) : (
+        (window.location.href = "/")
+      )}
     </BrowserRouter>
   );
 };
